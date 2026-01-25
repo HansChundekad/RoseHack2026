1c0d7b6ff1397d6dbbcf4deecf2f2bc99d7a6faf
Hans <hanschundekad@gmail.com>
2026-01-24 17:12:05 -0800

    updated get_all_loc sql code

--- vital-map/database/functions/get_all_locations_with_text.sql
+++ vital-map/database/functions/get_all_locations_with_text.sql
@@ -1,9 +1,3 @@
--- RPC Function: Get all locations with location converted to text
--- This function converts PostGIS geography to POINT text format
--- 
--- Usage: Call from Supabase client:
--- const { data } = await supabase.rpc('get_all_locations_with_text');
-
 CREATE OR REPLACE FUNCTION get_all_locations_with_text()
 RETURNS TABLE (
   id bigint,
@@ -23,7 +17,7 @@ BEGIN
     l.category,
     l.description,
     l.website_url,
-    ST_AsText(l.location::geometry) as location_text,
+    ST_AsText(ST_Transform(l.location, 4326)::geometry) as location_text,
     l.embedding,
     l.created_at
   FROM locations l
